4297dd3da90cfe076b6f20faa710cfa43e307f81
Regina <A00841923@tec.mx>
2025-09-15 11:19:04 -0600

    Update paint.py

--- paint.py
+++ paint.py
@@ -42,7 +42,18 @@ def circle(start, end):
 
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.x) / 2)
+        left(90)
+    
+    end_fill()
     pass  # TODO
 
 
